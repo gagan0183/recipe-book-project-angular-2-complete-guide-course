c261b2bfe2eb6d29eb85c2a1d0012005007c574a
gagan0183 <g.deepsingh1@gmail.com>
2017-10-31 10:00:49 +0530

    added routes to shopping list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
-export const APP_ROUTES_PROVIDERS: Routes = [
+const APP_ROUTES_PROVIDERS: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full'},
   { path: 'recipes', component: RecipesComponent},
   { path: 'shopping-list', component: ShoppingListComponent}
