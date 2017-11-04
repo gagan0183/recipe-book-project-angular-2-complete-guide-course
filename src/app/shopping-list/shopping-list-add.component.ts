bb0dd78e74d4cedcdc9b1853699aea5ae736a8c4
gagan0183 <g.deepsingh1@gmail.com>
2017-11-05 01:53:16 +0530

    comp

--- src/app/shopping-list/shopping-list-add.component.ts
+++ src/app/shopping-list/shopping-list-add.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, Input } from '@angular/core';
 import { Ingredient } from '../ingredient';
 
 import { ShoppingListService } from './shopping-list.service';
@@ -7,13 +7,20 @@ import { ShoppingListService } from './shopping-list.service';
   selector: 'rb-shopping-list-add',
   templateUrl: './shopping-list-add.component.html'
 })
-export class ShoppingListAddComponent implements OnInit {
+export class ShoppingListAddComponent implements OnChanges {
   isAdd = true;
-  item: Ingredient;
+  @Input() item: Ingredient;
 
   constructor(private sls: ShoppingListService) { }
 
-  ngOnInit() {
+  ngOnChanges(changes) {
+    if(changes.item.currentValue === null) {
+      this.isAdd = true;
+      this.item = {name: null, amount: null};
+    }
+    else {
+      this.isAdd = false;
+    }
   }
 
   onSubmit(ingredient: Ingredient) {
